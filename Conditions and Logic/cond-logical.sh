320beeb2d53bdeebcd57bda3e8582df8831b2d31
Jeph Mari Daligdig <122143538+greatxrider@users.noreply.github.com>
2023-09-07 23:20:40 +0800

    Add files via upload

---
+++ Conditions and Logic/cond-logical.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+#prompt user
+
+echo 'Are you enjoying this course so far?'
+echo -n "Enter \"y\" for yes, \"n\" for no"
+read response
+
+if [ "$response" == "y" ]
+then
+    echo "I'm pleased to hear you are enjoying the course!"
+    echo "Your feedback regarding what you have been enjoying would be most welcome!"
+elif [ "$response" = "n" ]
+then
+   echo "I'm sorry to hear you are not enjoying the course."
+   echo "Your feedback regarding what we can do to improve the learning experience"
+   echo "for this course would be greatly appreciated!"
+else
+   echo "Your response must be either 'y' or 'n'."
+   echo "Please re-run the script to try again."
+fi
+
+#Prompt user for integer and get product and sum
+
+echo -n "Enter an integer: "
+read n1
+echo -n "Enter another integer: "
+read n2
+sum=$(($n1+$n2))
+product=$(($n1*$n2))
+echo "The sum of $n1 and $n2 is $sum"
+echo "The product of $n1 and $n2 is $product."
+
+if [ $sum -lt $product ]
+then
+   echo "The sum is less than the product."
+elif [[ $sum == $product ]]
+then
+   echo "The sum is equal to the product."
+elif [ $sum -gt $product ]
+then
+   echo "The sum is greater than the product."
+fi
+
