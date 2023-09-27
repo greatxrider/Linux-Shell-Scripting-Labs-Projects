d1141187d17f997a4d4b1134b448dfbd883bc4bd
Jeph Mari Daligdig <122143538+greatxrider@users.noreply.github.com>
2023-09-27 18:12:14 +0800

    Add files via upload

---
+++ Basic Shell Scripts/greet.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+echo -n "Enter your name:"
+read name
+echo "Welcome $name"
+
+echo -n "Congratulations! You just created and ran your first shell script "
+echo "using Bash on IBM Skills Network"
---
+++ Basic Shell Scripts/greetnew.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+
+echo -n "Enter your first name:"
+read firstname
+
+echo -n "Enter your last name:"
+read lastname
+
+echo "Hello $firstname $lastname"
